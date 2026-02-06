878bc03957c66f43818d93c8077b495a223d2a3f
Firstname Lastname <filbs111@protonmail.com>
2026-02-06 23:55:02 +0000

    read uv coord info

--- blender_scripts/object_info.py
+++ blender_scripts/object_info.py
@@ -52,6 +52,7 @@ for obj in bpy.context.selected_objects:
       vertex_attribute_ids.append((deduped_positions[vectuple],None,deduped_normals[normtuple]))
         # None for tex coord. (TODO populate)
     
+    print(f"num vertices: {len(obj.data.vertices)}")
     print(f"num unique positions: {len(unique_positions_arr)}")
     print(f"num unique normals: {len(unique_normals_arr)}")
     
@@ -75,5 +76,33 @@ for obj in bpy.context.selected_objects:
             attrib_ids = vertex_attribute_ids[vert_idx]
             verts_data_strings.append('/'.join( map(str_or_empty, attrib_ids) ))
         print_and_write_to_file(f"f {' '.join(verts_data_strings)}")
+    
+    #because uv data might not exist
+    if len(mesh.uv_layers)>0:
+    
+        #check whether it is possible for multiple uv coords (for same layer) to exist on a single vertex. 
+        #seems that ordinarily only 1 uv coords val per vertex, but can get more by messing about with 
+        #uvs - mark seam, lightmap pack... 
+        #TODO create new verts that each have a single uv coord.
         
+        deduped_vert_uv_tuples = {}
+    
+        layer_zero_data = mesh.uv_layers[0].data
+        print(f"uv data length: {len(layer_zero_data)}")
+        print(f"loops length: {len(mesh.loops)}")
+        for loop in mesh.loops:
+            uv_loop = layer_zero_data[loop.index]
+            #print(f"uv: {uv_loop.uv}, vert: {loop.vertex_index}")
+            vert_uv_tuple = (uv_loop.uv[0],uv_loop.uv[1],loop.vertex_index)
+            if (vert_uv_tuple not in deduped_vert_uv_tuples):
+                deduped_vert_uv_tuples[vert_uv_tuple]=True
+        
+        print(f"num unique vertex, uv tuples: {len(deduped_vert_uv_tuples)}")
+    else:
+        print("no uv data!")
+     
+    #TODO include uv data in output.
+    
+    
+    
 save_file.close()
